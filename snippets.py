d785b9a5913e60e5319cad19b52152defc50c786
Nicole Nguyen <nicole.nguyen321@gmail.com>
2017-09-21 14:19:27 +0000

    Adding psycopg2

--- snippets.py
+++ snippets.py
@@ -1,5 +1,6 @@
 import logging
 import argparse
+import psycopg2
 
 # Set the log output file, and the log level
 # logging.basicConfig(filename="snippets.log", level1=logging.DEBUG, level2=logging.INFO)
@@ -8,6 +9,13 @@ logging.basicConfig(format='%(asctime)s %(message)s', filename="snippets.log", l
 logging.debug('Debug message for the log file')
 logging.info('Info message for the log file')
 logging.warning('Warning message for the log file')
+
+
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets")
+logging.debug("Database connection established.")
+
+
 
 # from bicycle class lesson - reference on how to use .format
 # class Bicycle(object):
@@ -19,33 +27,29 @@ logging.warning('Warning message for the log file')
 #     change 
 #     def __repr__(self):
 #         return "{} {}lbs ${}".format(self.brand, self.weight, self.prodCost)
-        
-        
-#class logging.LoggerAdapter(logger, extra)
-        
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--foo', help='foo help')
-args = parser.parse_args()
-        
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
 
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+# connect to database ( psycopg2 )
+
+def put(name, snippet):
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
-    
-    
-    
+
 def get(name):
     """Retrieve the snippet with a given name.
-
     If there is no such snippet, return '404: Snippet Not Found'.
-
     Returns the snippet.
     """
+
+    #retrieve the snippet from the db
+    
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    print("this function is running",get.__name__)
     return ""
     
 
@@ -53,21 +57,29 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    # put | get | anycommand | some take more arguments | --flags
+    put_parser = subparsers.add_parser('put', help="name message")
+    put_parser.add_argument('name', help="Name of snippet")
+    put_parser.add_argument('snippet', help="Snippet text")
+
+    get_parser = subparsers.add_parser('get', help="name to retrieve")
+    get_parser.add_argument('name', help="Name to be retreived from the database")
     
     # Subparser for the put command
     logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put", help="Store a snippet")
-    put_parser.add_argument("name", help="Name of the snippet")
-    put_parser.add_argument("snippet", help="Snippet text")
+    #put(name, snippet)    put_parser = subparsers.add_parser("put", help="Store a snippet")
+    #put_parser.add_argument("name", help="Name of the snippet")
+    #put_parser.add_argument("snippet", help="Snippet text")
     
     
     arguments = parser.parse_args()
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
-    command = arguments.pop("command")
     
+    command = arguments.pop("command")
+
     if command == "put":
         name, snippet = put(**arguments)
         print("Stored {!r} as {!r}".format(snippet, name))
@@ -75,6 +87,6 @@ def main():
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
         
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
     
